041dab216a36970f6891fd968358549e0e1c8fc7
Vraj <patelvap@gmail.com>
2019-05-12 22:10:51 -0400

    First Chapter Part 1

--- ch1/1-10.c
+++ ch1/1-10.c
@@ -1,5 +1,20 @@
 #include <stdio.h>
 
 int main() {
-    
+    int c;
+
+    while ((c=getchar()) != EOF) {
+        if (c == '\t') {
+            putchar('\\');
+            putchar('t');
+        }
+        else if (c == '\n') {
+            putchar('\\');
+            putchar('n');
+        }
+        else if (c == '\\') {
+            putchar('\\');
+            putchar('\\');
+        }
+    }
 }
---
+++ readme.md
@@ -0,0 +1,3 @@
+#Selected Exercises from The C Programming Language 2 end. by Kerningham and Ritchie
+
+Exercises were done while reading the book and uploaded to show growing proficiency in C.
